8db0608d1c94f395b58f3884b6f5438f02210e7d
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-11-28 09:51:39 +1000

    add functions it looks like I will need and their tests

---
+++ src/custom/getClientList.test.js
@@ -0,0 +1,3 @@
+it('gets the list of client applications for a tenant', ()=>{ 
+    return new Error('Not yet implemented')
+})
---
+++ src/custom/getRuleList.test.js
@@ -0,0 +1,7 @@
+it('gets a list of rules for a tenant', ()=>{ 
+    return new Error('Not yet implemented')
+})
+
+it('finds the applications mentioned in a rule',()=>{
+    return new Error('Not yet implemented')
+})
